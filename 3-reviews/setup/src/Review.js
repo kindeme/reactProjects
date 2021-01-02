f303ead647b9b6dbc78cebcdc0ae15d3446343ea
kindeme <fkounoudji@gmail.com>
2021-01-02 01:33:57 -0600

    random reviews

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -29,6 +29,13 @@ const Review = () => {
       return checkNumber(newIndex);
     });
   };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber == index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
 
   return (
     <article>
@@ -50,7 +57,9 @@ const Review = () => {
             <FaChevronRight />
           </button>
         </div>
-        <button className="random-btn">suprise me</button>
+        <button className="random-btn" onClick={randomPerson}>
+          suprise me
+        </button>
       </div>
     </article>
   );
