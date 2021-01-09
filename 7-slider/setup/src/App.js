1b1a0583a1016a15373414cb4a46ae69d124422f
kindeme <fkounoudji@gmail.com>
2021-01-09 15:54:09 -0600

    prev and next buttons

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -17,23 +17,30 @@ function App() {
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
           // more stuff comming up
-
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title"> {title}</p>
               <p className="text">{quote}</p>
-              <FaAngleDoubleRight className="icon" />
+              <FaQuoteRight className="icon" />
             </article>
           );
         })}
-        <button className="prev">
-          {" "}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
-          {" "}
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
