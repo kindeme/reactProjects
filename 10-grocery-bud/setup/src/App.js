0d31a8df879fa9767d7445a26d6e1125caf53243
kindeme <fkounoudji@gmail.com>
2021-01-12 19:59:13 -0600

    add items grocery

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -12,6 +12,19 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("hello");
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      //deal with edit
+    } else {
+      //show alert
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name,
+      };
+      setList([...list, newItem]);
+      setName("");
+    }
   };
 
   return (
@@ -32,12 +45,13 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn"> clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn"> clear items</button>
+        </div>
+      )}
     </section>
   );
 }
-
 export default App;
